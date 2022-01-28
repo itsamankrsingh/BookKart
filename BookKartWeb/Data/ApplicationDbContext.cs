9e32782db09fe329cf1e0b6e3250d7f771716a4c
itsamankrsingh <itsamankrsingh@gmail.com>
2022-01-28 23:29:00 +0530

    migration to database made

---
+++ BookKartWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookKartWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookKartWeb.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; } //create table in database name of table-> Categories
+    }
+}
