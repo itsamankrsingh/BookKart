59b67696c04a60d47e3f97cf22e2653ff3cf1680
itsamankrsingh <itsamankrsingh@gmail.com>
2022-01-30 00:35:32 +0530

    data fetched from database

---
+++ BookKartWeb/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using BookKartWeb.Data;
+using BookKartWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookKartWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db=db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
